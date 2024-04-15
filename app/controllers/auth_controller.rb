34acfeba86ee803464f7e0ce26e81433a92c7f90
Willian Binda <willianbinda122@gmail.com>
2024-04-15 16:06:57 -0300

    feat: fjkfbfa

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,28 +1,31 @@
 # app/controllers/auth_controller.rb
 class AuthController < ApplicationController
-  puts 'rota auth'
+  puts "\n\nrota auth\n\n"
+  # puts :authenticate_admin!
   before_action :authenticate_admin!
+  # puts "asfnasbfahs"
   skip_before_action :verify_authenticity_token
   
   def verify_token
+    puts "função auth"
     # if authorize_admin?()
-    head :ok
+    head :ok  
     # else
       # head :unauthorized
     # end
   end
 
-  private
+  # private
 
 
-  def authorize_admin?
-    puts 'entrou no AUTHHHHHHHHHHHHHHHHHHHH'
-    puts current_user.admin?
-    if current_user.admin?
-      return true
-    else
-      return false
-    end
-  end
+  # def authorize_admin?
+  #   puts 'entrou no AUTHHHHHHHHHHHHHHHHHHHH'
+  #   puts current_user
+  #   if current_user.admin?
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 
 end
